7fed7176123c737cf366ca712ada0ce7c0ab8744
Daryl Roy Santurdio <138319100+X-Secret-X@users.noreply.github.com>
2024-07-08 20:35:56 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+using VotingProject_Blazor.Components;
+using VotingProjectBlazor.Class;
+using VotingProjectBlazor.Services;
+using MudBlazor.Services;
+
+namespace VotingProject_Blazor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddScoped<AppDb>();
+            builder.Services.AddTransient<PositionServices>();
+            builder.Services.AddTransient<CandidatesServices>();
+            builder.Services.AddMudServices();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
